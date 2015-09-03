a3dcc9a306408e7fdd5653dbd2c64b1e92369575
Paul Mooney <paul@daishisystems.com>
2015-09-03 14:26:31 +0100

    Added PayPalAdapter documentation

--- Daishi.PaySharp/PayPalAdapter.cs
+++ Daishi.PaySharp/PayPalAdapter.cs
@@ -12,12 +12,28 @@ using System.Web;
 
 namespace Daishi.PaySharp {
     /// <summary>
-    /// <
+    ///     PayPalAdapter interfaces with PayPal HTTP endpoints and provides both
+    ///     synchronous and asynchronous mechanisms that consume those endpoints.
+    ///     <remarks>
+    ///         PayPal exposes metadata in a form-urlencoded PayPalAdapter
+    ///         provides a means to retrieve PayPal metadata in raw-format.
+    ///     </remarks>
     /// </summary>
     public class PayPalAdapter {
-        // todo: Document and then add DoExpressCheckout
+
+        /// <summary>
+        ///     Executes PayPal's SetExpressCheckout function in order to return a
+        ///     PayPal Access Token.
+        /// </summary>
+        /// <param name="payload">
+        ///     Metadata necessary to facilitate a successful SetExpressCheckout call.
+        ///     Payload will be converted to key-value format.
+        /// </param>
+        /// <param name="encoding">Text encoding to apply during byte-to-text conversion.</param>
+        /// <param name="expressCheckoutURI">Default PayPal ExpressCheckout HTTP URI.</param>
+        /// <returns>Raw metadata, in key-value format, containing a PayPal Access Token.</returns>
         public string SetExpressCheckout(SetExpressCheckoutPayload payload,
-            Encoding encoding, string setExpressCheckoutURI) {
+            Encoding encoding, string expressCheckoutURI) {
 
             var nvc = new NameValueCollection {
                 {"USER", payload.User},
@@ -34,13 +50,17 @@ namespace Daishi.PaySharp {
 
             using (var webClient = new WebClient()) {
 
-                var response = webClient.UploadValues(setExpressCheckoutURI, nvc);
+                var response = webClient.UploadValues(expressCheckoutURI, nvc);
                 return encoding.GetString(response);
             }
         }
 
+        /// <summary>
+        ///     SetExpressCheckout asynchronous equivalent.
+        ///     <seealso cref="SetExpressCheckout" />
+        /// </summary>
         public async Task<string> SetExpressCheckoutAsync(SetExpressCheckoutPayload payload,
-            Encoding encoding, string setExpressCheckoutURI) {
+            Encoding encoding, string expressCheckoutURI) {
 
             var nvc = new NameValueCollection {
                 {"USER", payload.User},
@@ -57,13 +77,23 @@ namespace Daishi.PaySharp {
 
             using (var webClient = new WebClient()) {
 
-                var response = await webClient.UploadValuesTaskAsync(setExpressCheckoutURI, nvc);
+                var response = await webClient.UploadValuesTaskAsync(expressCheckoutURI, nvc);
                 return encoding.GetString(response);
             }
         }
 
+        /// <summary>
+        ///     Executes PayPal's GetExpressCheckoutDetails function in order to
+        ///     return PayPal Customer Details.
+        /// </summary>
+        /// <param name="payload">
+        ///     Metadata necessary to facilitate a successful GetExpressCheckoutDetails call.
+        ///     Payload will be converted to key-value format.
+        /// </param>
+        /// <param name="expressCheckoutUri">Default PayPal ExpressCheckout HTTP URI.</param>
+        /// <returns>Raw metadata, in key-value format, containing PayPal Customer Details.</returns>
         public string GetExpressCheckoutDetails(
-            GetExpressCheckoutDetailsPayload payload, string getExpressCheckoutUri) {
+            GetExpressCheckoutDetailsPayload payload, string expressCheckoutUri) {
 
             var nvc = new NameValueCollection {
                 {"USER", payload.User},
@@ -80,10 +110,14 @@ namespace Daishi.PaySharp {
             using (var webClient = new WebClient()) {
 
                 return webClient.DownloadString(
-                    new Uri(string.Concat(getExpressCheckoutUri, "?", queryString)));
+                    new Uri(string.Concat(expressCheckoutUri, "?", queryString)));
             }
         }
 
+        /// <summary>
+        ///     GetExpressCheckoutDetails asynchronous equivalent.
+        ///     <seealso cref="GetExpressCheckoutDetails" />
+        /// </summary>
         public async Task<string> GetExpressCheckoutDetailsAsync(
             GetExpressCheckoutDetailsPayload payload, string getExpressCheckoutUri) {
 
