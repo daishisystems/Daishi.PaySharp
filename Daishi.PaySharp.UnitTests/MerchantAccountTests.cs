667fa43fd631accd6f42290da4fc5ea7dff8897a
Paul Mooney <paul@daishisystems.com>
2015-09-07 11:40:16 +0100

    Added Merchant Account management feature.

---
+++ Daishi.PaySharp.UnitTests/MerchantAccountTests.cs
@@ -0,0 +1,31 @@
+﻿#region Includes
+
+using NUnit.Framework;
+
+#endregion
+
+namespace Daishi.PaySharp.UnitTests {
+    [TestFixture]
+    internal class MerchantAccountTests {
+
+        [Test]
+        public void MerchantAccountIDIsAccessibleByCurrencyCode() {
+
+            var merchantAccounts = new MerchantAccounts();
+            merchantAccounts.Load("GBP", "CNWKRL2JZLMPL");
+
+            var merchantAccountID = merchantAccounts.GetByCurrencyCode("GBP");
+            Assert.AreEqual("CNWKRL2JZLMPL", merchantAccountID);
+        }
+
+        [Test]
+        public void InvalidCurrencyCodeResultsInEmptyMerchantAccountID() {
+
+            var merchantAccounts = new MerchantAccounts();
+            merchantAccounts.Load("GBP", "CNWKRL2JZLMPL");
+
+            var merchantAccountID = merchantAccounts.GetByCurrencyCode("INVALID");
+            Assert.AreEqual(string.Empty, merchantAccountID);
+        }
+    }
+}
---
+++ Daishi.PaySharp/MerchantAccount.cs
@@ -0,0 +1,58 @@
+﻿#region Includes
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace Daishi.PaySharp {
+    /// <summary>
+    ///     MerchantAccounts provides a means of managing PayPal Merchant Accounts,
+    ///     by loading Merchant Account metadata from external sources, such as
+    ///     *.config files, and by providing access to underlying Merchant Account
+    ///     metadata through key-value lookup.
+    /// </summary>
+    public class MerchantAccounts {
+        private readonly Dictionary<string, string> _merchantAccounts =
+            new Dictionary<string, string>();
+
+        /// <summary>
+        ///     Loads Merchant Account metadata.
+        ///     <remarks>
+        ///         Metadata is internally stored in key-value format to
+        ///         facilitate fast lookup.
+        ///     </remarks>
+        /// </summary>
+        /// <param name="currencyCode">
+        ///     The currency-code pertaining to the desired Merchant Account.
+        /// </param>
+        /// <param name="merchantAccountID">Merchant Account Identifier.</param>
+        public void Load(string currencyCode, string merchantAccountID) {
+
+            _merchantAccounts.Add(currencyCode, merchantAccountID);
+        }
+
+        /// <summary>
+        ///     Returns a Merchant Account ID pertaining to the specified
+        ///     currency-code.
+        ///     <remarks>
+        ///         Returns an empty string if the Merchant Account cannot be
+        ///         found.
+        ///     </remarks>
+        /// </summary>
+        /// <param name="currencyCode">
+        ///     The currency-code pertaining to the desired Merchant Account.
+        /// </param>
+        /// <returns>
+        ///     A Merchant Account ID pertaining to <c>currencyCode</c>, if
+        ///     <c>currencyCode</c> is valid. Otherwise, an empty <see cref="string" />.
+        /// </returns>
+        public string GetByCurrencyCode(string currencyCode) {
+
+            string merchantAccountID;
+            var merchantAccountExists =
+                _merchantAccounts.TryGetValue(currencyCode, out merchantAccountID);
+
+            return !merchantAccountExists ? string.Empty : merchantAccountID;
+        }
+    }
+}
