f6ef149285edc4aabfb4a1a6eb29a9518750ed26
Paul Mooney <paul@daishisystems.com>
2015-09-17 11:37:59 +0100

    Added PayPalPayload Bridge Abstraction

--- Daishi.PaySharp/Extensibility/PayPalPayload.cs
+++ Daishi.PaySharp/Extensibility/PayPalPayload.cs
@@ -1,6 +1,25 @@
 ﻿namespace Daishi.PaySharp.Extensibility {
     /// <summary>
-    /// 
+    ///     Provides a
+    ///     <a href="http://www.dofactory.com/net/bridge-design-pattern">Bridge</a>
+    ///     that allows downstream systems to interface with and expose PayPal
+    ///     ExpressCheckout metadata.
     /// </summary>
-    public class PayPalPayload {}
+    public abstract class PayPalPayload {
+        /// <summary>The underlying <see cref="CustomerDetails" />
+        ///     <a href="http://www.oodesign.com/bridge-pattern.html">Implementor</a>
+        ///     that facilitates this
+        ///     <a href="http://www.dofactory.com/net/bridge-design-pattern">Bridge</a>.</summary>
+        protected readonly CustomerDetails customerDetails;
+
+        /// <summary>Initializes a new instance of the <see cref="PayPalPayload" /> class.</summary>
+        /// <param name="customerDetails">The underlying <see cref="CustomerDetails" />
+        ///     <a href="http://www.oodesign.com/bridge-pattern.html">Implementor</a>
+        ///     that facilitates this
+        ///     <a href="http://www.dofactory.com/net/bridge-design-pattern">Bridge</a>.</param>
+        protected PayPalPayload(CustomerDetails customerDetails) {
+
+            this.customerDetails = customerDetails;
+        }
+    }
 }
