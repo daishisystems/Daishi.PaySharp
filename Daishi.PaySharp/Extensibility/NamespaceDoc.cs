429914e4235b3ce9323c5a0fbc57bc289ece0046
Paul Mooney <paul@daishisystems.com>
2015-09-18 10:09:56 +0100

    Added extensibility documentation

---
+++ Daishi.PaySharp/Extensibility/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿namespace Daishi.PaySharp.Extensibility {
+    /// <summary>
+    ///     Provides functionality that extends PayPal metadata to interface with
+    ///     downstream systems, such as:
+    ///     <para>
+    ///         <list type="bullet">
+    ///             <item>Fraud Prevention</item>
+    ///             <item>Financial and Accounting</item>
+    ///             <item>Booking and Reservation</item>
+    ///         </list>
+    ///     </para>
+    /// </summary>
+    public static class NamespaceDoc {}
+}
--- Daishi.PaySharp/Extensibility/PayPalPayload.cs
+++ Daishi.PaySharp/Extensibility/PayPalPayload.cs
@@ -3,7 +3,12 @@
     ///     Provides a
     ///     <a href="http://www.dofactory.com/net/bridge-design-pattern">Bridge</a>
     ///     that allows downstream systems to interface with and expose PayPal
-    ///     ExpressCheckout metadata.
+    ///     <b>ExpressCheckout</b> metadata.
+    ///     <remarks>
+    ///         Downstream systems may use this class to absorb a
+    ///         <see cref="CustomerDetails" /> and transform it into a type suitable
+    ///         for consumption by downstream systems.
+    ///     </remarks>
     /// </summary>
     public abstract class PayPalPayload {
         /// <summary>The underlying <see cref="CustomerDetails" />
